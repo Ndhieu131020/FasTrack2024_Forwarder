6fc3a6d3f6115a70056eb3fc85551ddaa1f672b7
Phong.PT <phong.pt.hust@gmail.com>
2024-11-17 12:21:33 +0700

    Update Queue

--- src/middleware/src/MID_Queue_Interface.c
+++ src/middleware/src/MID_Queue_Interface.c
@@ -13,10 +13,19 @@ static CircularQueueReceive_t receiveQueue;
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
+/* Check if QueueReceive is full */
+static uint8_t QueueReceive_isFull( const CircularQueueReceive_t *const pQueue );
 
+/* Check if QueueReceive is empty */
+static uint8_t QueueReceive_isEmpty( const CircularQueueReceive_t *const pQueue );
+
+/* Initialize for QueueReceive */
 void MID_Receive_Queue_Init( void )
 {
-    QueueReceive_Init(&receiveQueue);
+    receiveQueue.front    = -1;
+    receiveQueue.rear     = -1;
+    receiveQueue.capacity = 0U;
+    receiveQueue.size     = QUEUE_RECEIVE_SIZE;
 }
 
 
@@ -26,7 +35,25 @@ QueueCheckOperation_t MID_Receive_EnQueue( const ReceiveFrame_t *const pInData )
 
     if( pInData != NULL )
     {
-    	status = QueueReceive_enQueue(&receiveQueue, pInData);
+        if ( !QueueReceive_isFull(&receiveQueue) )
+        {
+            if ( receiveQueue.front == -1 )
+            {
+            	receiveQueue.front = 0;
+            }
+            else
+            {
+                /* Do Nothing */
+            }
+            receiveQueue.rear = ((receiveQueue.rear) + 1) % (receiveQueue.size);
+            receiveQueue.queueArray[receiveQueue.rear] = *pInData;
+            (receiveQueue.capacity)++;
+            status = QUEUE_DONE_SUCCESS;
+        }
+        else
+        {
+            /* Do Nothing */
+        }
     }
     else
     {
@@ -43,7 +70,73 @@ QueueCheckOperation_t MID_Receive_DeQueue( ReceiveFrame_t *const pOutData )
 
     if( pOutData !=NULL )
     {
-        status = QueueReceive_deQueue(&receiveQueue, pOutData);
+        if ( !QueueReceive_isEmpty(&receiveQueue) )
+        {
+            *pOutData = receiveQueue.queueArray[receiveQueue.front];
+            (receiveQueue.capacity)--;
+            if (receiveQueue.front == receiveQueue.rear)
+            {
+            	receiveQueue.front = -1;
+            	receiveQueue.rear  = -1;
+            }
+            else
+            {
+            	receiveQueue.front = ((receiveQueue.front) + 1) % (receiveQueue.size);
+            }
+            status = QUEUE_DONE_SUCCESS;
+        }
+        else
+        {
+        	/* Do Nothing */
+        }
+    }
+    else
+    {
+        /* Do Nothing */
+    }
+
+    return status;
+}
+
+
+static uint8_t QueueReceive_isFull( const CircularQueueReceive_t *const pQueue )
+{
+    uint8_t status = FALSE;
+
+    if( pQueue != NULL )
+    {
+        if( (pQueue -> capacity) == (pQueue -> size) )
+        {
+            status = TRUE;
+        }
+        else
+        {
+            /* Do Nothing */
+        }
+    }
+    else
+    {
+        /* Do Nothing */
+    }
+
+    return status;
+}
+
+
+static uint8_t QueueReceive_isEmpty( const CircularQueueReceive_t *const pQueue )
+{
+    uint8_t status = FALSE;
+
+    if( pQueue != NULL )
+    {
+        if( pQueue->capacity == 0U )
+        {
+            status = TRUE;
+        }
+        else
+        {
+            /* Do Nothing */
+        }
     }
     else
     {
