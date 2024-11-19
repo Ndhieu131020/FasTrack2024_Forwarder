71c95ef8d26cbe1ea17e3767b87ad9bdbd31ba61
HIEUND66 <ndhieu131020@gmail.com>
2024-11-19 10:05:59 +0700

    Backup

--- src/middleware/inc/MID_UART_Interface.h
+++ src/middleware/inc/MID_UART_Interface.h
@@ -8,6 +8,23 @@
 /*******************************************************************************
  * Definition
  ******************************************************************************/
+/** @defgroup Check Connection Message ID
+  * @{
+  */
+#define PC_CONNECT_FORWARDER_ID         0xA0
+#define PC_CONNECT_DISTANCE_SENSOR_ID   0xA1
+#define PC_CONNECT_ROTATION_SENSOR_ID   0xA2
+
+#define REQUEST_CONNECTION_DATA         0x10
+#define CONFIRM_CONNECTION_DATA         0xFF
+
+/** @defgroup Data Message ID
+  * @{
+  */
+#define ROTATION_DATA_ID   0xC0
+#define DISTANCE_DATA_ID   0xD0
+
+#define CONFIRM_SENSOR_DATA    0xFFFF
 
 /*******************************************************************************
  * API
